b39ea32b470cbe811e3d475cee31319b45a43b51
jonathan <gaertnerjonathan@gmail.com>
2020-07-08 19:50:27 +0200

    Base Functions of CA working

--- CellularAutomat.py
+++ CellularAutomat.py
@@ -8,22 +8,21 @@ import sys
 
 
 class HumanBeing:
-    def __init__(self, city: list, simu, contact_restriction: int, infected: bool = False, naughty: bool = False,
+    def __init__(self, simu, contact_restriction: int, infected: bool = False, naughty: bool = False,
                  immune: int = 0):
         self.naughty = naughty
         self.infected = infected
-        self.city = city
         self.contact_restriction = contact_restriction
         self.immunity = immune
         self.simu = simu
         self.step()
 
     def step(self):
-        home = random.choice(self.city)
+        home = random.choice(self.simu.city)
         while not self.naughty:
-            if len(home) < self.contact_restriction:
+            if len(home) <= self.contact_restriction:
                 break
-            home = random.choice(self.city)
+            home = random.choice(self.simu.city)
         home.append(self)
 
         if self.immunity:
@@ -35,7 +34,7 @@ class HumanBeing:
 
 
 class Simulation:
-    def __init__(self, persons: int, houses: int = 30, contact_restrictions: int = 0, infected_start: int = 1,
+    def __init__(self, persons: int, houses: int = 10, contact_restrictions: int = 0, infected_start: int = 1,
                  naughty_start: int = 1, recovered_start: int = 0, immunity_time: int = sys.maxsize,
                  disinfection_prob: int = 5, infection_prob: int = 0.9):
         if not contact_restrictions:
@@ -45,7 +44,7 @@ class Simulation:
         self.immunity_time = immunity_time
         self.houses = houses
         self.city = [[] for _ in range(self.houses)]
-        self.human_beings = [HumanBeing(self.city, self, contact_restrictions) for _ in range(persons)]
+        self.human_beings = [HumanBeing(self, contact_restrictions) for _ in range(persons)]
         for _ in range(infected_start):
             random.choice(self.human_beings).infected = True
         for _ in range(naughty_start):
@@ -69,7 +68,7 @@ class Simulation:
         for index in self.human_beings:
             if index.immunity:
                 self.r += 1
-        self.s = len(self.human_beings) - self.i - self.r
+        self.s = (len(self.human_beings) - self.i) - self.r
 
         self.city = [[] for _ in range(self.houses)]
 
@@ -80,7 +79,7 @@ class Simulation:
             states = [i.infected for i in house]
             if True in states:
                 for i in house:
-                    if random.randint(0.0, self.infection_prob) == 1:
+                    if random.randint(0, int(self.infection_prob*10)) == 1 and not i.immunity:
                         i.infected = True
 
         return self.s, self.i, self.r
@@ -93,7 +92,7 @@ class Simulation:
         for i in range(steps):
             states = self.step()
 
-            susceptible.apend(states[0])
+            susceptible.append(states[0])
             infected.append(states[1])
             recovered.append(states[2])
 
--- CellularAutomat_plot.py
+++ CellularAutomat_plot.py
@@ -1,6 +1,13 @@
 import CellularAutomat
+from matplotlib import pyplot as plt
 
+steps = 100
 CA = CellularAutomat.Simulation(100)
 
-for i in range(0, 100):
-    print(CA.step())
+susceptible, infected, recovered = CA.steps(steps)
+xaxis = list(range(steps))
+
+plt.plot(xaxis, susceptible, "k")
+plt.plot(xaxis, infected, "b")
+plt.plot(xaxis, recovered, "r")
+plt.show()
