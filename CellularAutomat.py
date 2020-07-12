8c669bde322e4521050174338ffa6f158865ffdc
jonathan <gaertnerjonathan@gmail.com>
2020-07-12 20:32:38 +0200

    adding rising of naughty people (needs to be optimized)

--- CellularAutomat.py
+++ CellularAutomat.py
@@ -36,20 +36,43 @@ class HumanBeing:
 class Simulation:
     def __init__(self, persons: int, houses: int = 30, contact_restrictions: int = 0, infected_start: int = 1,
                  naughty_start: int = 1, recovered_start: int = 0, immunity_time: int = True,
-                 disinfection_prob: float = 0.1, infection_prob: float = 0.9, vaccine_per_tick: int = 0):
+                 disinfection_prob: float = 0.1, infection_prob: float = 0.9, vaccine_per_tick: int = 0,
+                 naughty_plus: int = 2):
+        """
+        :param persons: Population which will be simulated
+        :param houses: diffrent places to go will get more complicated. Can also be seen as 2 meter square areas
+        :param contact_restrictions: how many people are allowed to meet will be ignored by the naughty people
+        :param infected_start: how much people are infected at the beginning
+        :param naughty_start: how much people do not care about the rules at the beginning
+        :param recovered_start: how much people are already immune
+        :param immunity_time: how long are the people immune. Does the immunity even stop
+        :param disinfection_prob: how high probability to recover and get immune
+        :param infection_prob: how high is the probability to get infected by a person in one tick (1-)
+        :param vaccine_per_tick: how many vaccines are available per tick
+        :param naughty_plus: How many people get suddenly naughty if there is a positive change for them
+        """
         if not contact_restrictions:
             contact_restrictions = persons
+        if recovered_start + infected_start >= persons:
+            raise Exception
         self.disinfection = disinfection_prob
         self.infection_prob = infection_prob
         self.immunity_time = immunity_time
         self.vaccine_per_tick = vaccine_per_tick
+        self.naughty_plus = naughty_plus
+        self.naughty = naughty_start
         self.houses = houses
         self.city = [[] for _ in range(self.houses)]  # Could also be interpreted as many 5 meter zones
         self.human_beings = [HumanBeing(self, contact_restrictions) for _ in range(persons)]
-        for human in random.sample(infected_start):  # choosing people with special roles for the beginning
+        for human in random.sample(self.human_beings, infected_start):  # choosing people with special roles for the beginning
             human.infected = True
         for _ in range(naughty_start):
-            random.choice(self.human_beings).naughty = True
+            human = random.choice(self.human_beings)
+            timer = 0
+            while human.naughty and timer != 2 * len(self.human_beings):
+                human = random.choice(self.human_beings)
+                timer += 1
+            human.naughty = True
         for _ in range(recovered_start):
             human = random.choice(self.human_beings)
             while human.infected:
@@ -60,18 +83,25 @@ class Simulation:
         self.i = infected_start
         self.r = recovered_start
 
-    def step(self) -> tuple:
-        self.s, self.i, self.r = 0, 0, 0
-        for index in self.human_beings:
-            if index.infected:
-                self.i += 1
-
-        for index in self.human_beings:
-            if index.immunity:
-                self.r += 1
-        self.s = (len(self.human_beings) - self.i) - self.r
-
+    def step(self, change: bool = False) -> tuple:
         self.city = [[] for _ in range(self.houses)]
+
+        if change:
+            for _ in range(self.naughty_plus):
+                human = random.choice(self.human_beings)
+                timer = 0
+                while human.naughty and timer != 2 * len(self.human_beings):
+                    human = random.choice(self.human_beings)
+                    timer += 1
+                human.naughty = True
+
+        for _ in range(self.naughty):
+            human = random.choice(self.human_beings)
+            timer = 0
+            while human.naughty and timer != 2 * len(self.human_beings):
+                human = random.choice(self.human_beings)
+                timer += 1
+            human.naughty = True
 
         for i in self.human_beings:
             i.step()
@@ -86,15 +116,28 @@ class Simulation:
             states = [i.infected for i in house]
             if True in states:
                 for i in house:
-                    if random.randint(0, int(self.infection_prob*100)) == 0 and not i.immunity:
+                    if random.randint(0, int(self.infection_prob * 100)) == 0 and not i.immunity:
                         i.infected = True
 
-        return self.s, self.i, self.r
+        self.s, self.i, self.r = 0, 0, 0  # Will be recalculated every step
+        for index in self.human_beings:
+            if index.infected:
+                self.i += 1
+
+        for index in self.human_beings:
+            if index.immunity:
+                self.r += 1
+        self.s = (len(self.human_beings) - self.i) - self.r
+
+        self.naughty = len([i.naughty for i in self.human_beings if i.naughty])
+
+        return self.s, self.i, self.r, self.naughty
 
     def steps(self, steps):  # For plots mainly
         susceptible = []
         infected = []
         recovered = []
+        naughty = []
 
         for i in range(steps):
             states = self.step()
@@ -102,5 +145,6 @@ class Simulation:
             susceptible.append(states[0])
             infected.append(states[1])
             recovered.append(states[2])
+            naughty.append(states[3])
 
-        return susceptible, infected, recovered
+        return susceptible, infected, recovered, naughty
--- CellularAutomat_plot.py
+++ CellularAutomat_plot.py
@@ -6,26 +6,28 @@ persons = 100
 houses = 30
 contact_restrictions = 0  # If no restriction planned set to 0
 infected_start = 1
-naughty_start = 1
+naughty_start = 2
 recovered_start = 0
 immunity_time = True  # Never expires
 disinfection_prob = 1-0.1
 infection_prob = 1-0.9
-vaccine_per_tick = 0
+vaccine_per_tick = 2
+naughty_plus = 2
 
 health_system_border = 10
 
 CA = CellularAutomat.Simulation(persons, houses, contact_restrictions, infected_start, naughty_start, recovered_start,
-                                immunity_time, disinfection_prob, infection_prob, vaccine_per_tick)
+                                immunity_time, disinfection_prob, infection_prob, vaccine_per_tick, naughty_plus)
 
-susceptible, infected, recovered = CA.steps(steps)
+susceptible, infected, recovered, naughty = CA.steps(steps)
 xaxis = list(range(steps))
 health_system = [health_system_border for _ in range(steps)]
 
 plt.plot(xaxis, health_system, "o", label="health system border")
 plt.plot(xaxis, susceptible, "k", label="susceptible")
 plt.plot(xaxis, infected, "b", label="infected")
-plt.plot(xaxis, recovered, "r", label="recovered")
+plt.plot(xaxis, recovered, "g", label="recovered")
+plt.plot(xaxis, naughty, "r", label="naughty")
 plt.legend()
 plt.title("COVID-19 simulation")
 plt.xlabel("time")
